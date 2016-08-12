9e8dec6c09ee806feb1a825726afebc837303df2
craigmiller160 <craigmiller160@gmail.com>
2016-08-11 22:25:08 -0400

    Documented the Dao interface and provided an abstract implementation.

---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/AbstractDao.java
@@ -0,0 +1,37 @@
+package io.craigmiller160.orgbuilder.server.data;
+
+import io.craigmiller160.orgbuilder.server.logging.OrgApiLogger;
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * An abstract implementation of the Dao interface.
+ * It provides an implementation for the query()
+ * method, which uses reflection to identify which
+ * method to invoke.
+ *
+ * Created by craig on 8/11/16.
+ *
+ * @param <E> the type of the element this DAO persists.
+ * @param <I> the type of the primary key, or ID, of the
+ *           element this DAO persists.
+ */
+public abstract class AbstractDao<E,I> implements Dao<E,I> {
+
+    @Override
+    public List<E> query(String queryName, Object... params) {
+        Object result = null;
+        try{
+            result = MethodUtils.invokeMethod(this, queryName, params);
+        }
+        catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException ex){
+            OrgApiLogger.getDataLogger().error("Unable to invoke query method. QueryName: " + queryName + " Params: " + Arrays.toString(params), ex);
+        }
+
+        //noinspection unchecked
+        return (List<E>) result;
+    }
+}
--- server/src/main/java/io/craigmiller160/orgbuilder/server/data/Dao.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/Dao.java
@@ -3,24 +3,111 @@ package io.craigmiller160.orgbuilder.server.data;
 import java.util.List;
 
 /**
+ * The public API of all DAO objects in this application.
+ * This interface defines methods that handle all key CRUD
+ * operations.
+ *
+ * In order to support more flexible implementations, it
+ * also contains the query() method. Implementation classes
+ * will likely have a wide range of possible operations to
+ * perform. Rather than referencing those implementations
+ * directly, the query() method can be used to call into those
+ * other methods. The implementing classes will determine how
+ * this is done, however all methods must return a List<E>
+ * to match the query() method's return type.
+ *
  * Created by craig on 8/10/16.
+ *
+ * @param <E> the type of the element this DAO persists.
+ * @param <I> the type of the primary key, or ID, of the
+ *           element this DAO persists.
  */
 public interface Dao<E,I> {
 
-    void insert(E element);
+    /**
+     * Insert the provided element into the database.
+     *
+     * @param element the element to be inserted.
+     * @return the element, after being inserted.
+     */
+    E insert(E element);
 
-    void update(E element);
+    /**
+     * Update the provided element in the database.
+     *
+     * @param element the element to update.
+     * @return the element, after being updated.
+     */
+    E update(E element);
 
-    void delete(I id);
+    /**
+     * Delete the element with the provided ID from
+     * the database.
+     *
+     * @param id the ID of the element.
+     * @return the element that was deleted.
+     */
+    E delete(I id);
 
-    E get(I id, boolean withJoins);
+    /**
+     * Get the element with the provided ID from
+     * the database.
+     *
+     * @param id the ID of the element.
+     * @return the element that was retrieved.
+     */
+    E get(I id);
 
+    /**
+     * Get the element with the provided ID
+     * from the database. If other elements are joined
+     * with it, they will be retrieved along with it.
+     *
+     * @param id the ID of the element.
+     * @return the element that was retrieved.
+     */
+    E getWithJoins(I id);
+
+    /**
+     * Get the count of the total number of
+     * elements of the type managed by this
+     * DAO in the database.
+     *
+     * @return the count of the elements of
+     *          this type in the database.
+     */
     int getCount();
 
-    List<E> getAll(boolean withJoins);
+    /**
+     * Get all of the elements of the type
+     * managed by this DAO in the database.
+     *
+     * @return all of the elements of this type in
+     *          the database.
+     */
+    List<E> getAll();
 
-    List<E> getAll(boolean withJoins, int offset, int limit);
+    /**
+     * Get all of the elements of the type
+     * managed by this DAO in the database,
+     * limited by the offset and limit parameters.
+     *
+     * @param offset the offset to start getting elements at.
+     * @param limit the limit of how many elements to retrieve.
+     * @return all of the elements of this type between the
+     *          offset and limit in the database.
+     */
+    List<E> getAll(int offset, int limit);
 
+    /**
+     * Invoke an additional query method in the
+     * implementation of this interface, that matches
+     * the provided name and parameters.
+     *
+     * @param queryName the name of the query method.
+     * @param params the parameters for the query method.
+     * @return a list of the element results.
+     */
     List<E> query(String queryName, Object...params);
 
 }
