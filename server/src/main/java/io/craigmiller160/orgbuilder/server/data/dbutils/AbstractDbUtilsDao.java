8c1252e9642dec3b2d640fd141341ed622fe38ee
craigmiller160 <craigmiller160@gmail.com>
2016-08-17 12:42:10 -0400

    Stubbed out AbstractDbUtilsDao, with stubbed methods for handling the common code for all DAO implementations.

---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/dbutils/AbstractDbUtilsDao.java
@@ -0,0 +1,25 @@
+package io.craigmiller160.orgbuilder.server.data.dbutils;
+
+import io.craigmiller160.orgbuilder.server.data.AbstractDao;
+
+/**
+ * Created by craigmiller on 8/17/16.
+ */
+public abstract class AbstractDbUtilsDao<E,I> extends AbstractDao<E,I> {
+
+    protected E executeUpdate(){
+        //TODO finish this
+        return null;
+    }
+
+    protected E executeQuery(){
+        //TODO finish this
+        return null;
+    }
+
+    protected E executeBatch(){
+        //TODO finish this
+        return null;
+    }
+
+}
