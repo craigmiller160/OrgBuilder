cc5aefe20639b2ccbc9f88b48106e50cbc603cfe
craigmiller160 <craigmiller160@gmail.com>
2016-08-17 20:25:08 -0400

    Improved DaoFactoryProvider and DbUtilsDaoFactory, including creating the OrgDataSource class.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/data/DaoFactoryProvider.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/DaoFactoryProvider.java
@@ -1,26 +1,26 @@
 package io.craigmiller160.orgbuilder.server.data;
 
+import io.craigmiller160.orgbuilder.server.ServerCore;
+import io.craigmiller160.orgbuilder.server.ServerProps;
+
 /**
  * Created by craig on 8/10/16.
  */
 public class DaoFactoryProvider {
 
-    //TODO move this to a properties file
-    private static final String DEFAULT_FACTORY = "io.craigmiller160.orgbuilder.server.data.dbutils.DbUtilsDaoFactory";
-
     public static DaoFactory newDefaultFactory(){
-        return newFactory(DEFAULT_FACTORY);
+        String defaultFactory = ServerCore.properties.getProperty(ServerProps.DEFAULT_DAO_PROP);
+        return newFactory(defaultFactory);
     }
 
-    public static DaoFactory newFactory(String className){
+    public static DaoFactory newFactory(String className) {
         DaoFactory daoFactory = null;
-        try{
+        try {
             Class<?> clazz = Class.forName(className);
-            if(clazz != null && clazz.isAssignableFrom(DaoFactory.class)){
+            if (clazz != null && clazz.isAssignableFrom(DaoFactory.class)) {
                 daoFactory = (DaoFactory) clazz.newInstance();
             }
-        }
-        catch(ClassNotFoundException | InstantiationException | IllegalAccessException ex){
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             //TODO handle this
         }
 
--- server/src/main/java/io/craigmiller160/orgbuilder/server/data/dbutils/DbUtilsDaoFactory.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/dbutils/DbUtilsDaoFactory.java
@@ -1,10 +1,17 @@
 package io.craigmiller160.orgbuilder.server.data.dbutils;
 
+import io.craigmiller160.orgbuilder.server.ServerCore;
+import io.craigmiller160.orgbuilder.server.ServerProps;
 import io.craigmiller160.orgbuilder.server.data.Dao;
 import io.craigmiller160.orgbuilder.server.data.DaoFactory;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.StringUtils;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by craig on 8/10/16.
@@ -22,5 +29,39 @@ public class DbUtilsDaoFactory implements DaoFactory {
     public <E, I> Dao<E, I> newDao(Class<Dao<E, I>> clazz) {
         //noinspection unchecked
         return daoMap.get(clazz);
+    }
+
+    private static class OrgDataSource {
+
+        private final BasicDataSource dataSource;
+
+        public OrgDataSource(){
+            dataSource = new BasicDataSource();
+            String clazz = ServerCore.properties.getProperty(ServerProps.DB_CLASS_PROP);
+            String url = ServerCore.properties.getProperty(ServerProps.DB_URL_PROP);
+            String user = ServerCore.properties.getProperty(ServerProps.DB_USER_PROP);
+            String pass = ServerCore.properties.getProperty(ServerProps.DB_PASS_PROP);
+            String initString = ServerCore.properties.getProperty(ServerProps.POOL_INIT_SIZE_PROP);
+            String maxString = ServerCore.properties.getProperty(ServerProps.POOL_MAX_SIZE_PROP);
+            int init = StringUtils.isNumeric(initString) ? Integer.parseInt(initString) : -1;
+            int max = StringUtils.isNumeric(maxString) ? Integer.parseInt(maxString) : -1;
+
+            dataSource.setDriverClassName(clazz);
+            dataSource.setUrl(url);
+            dataSource.setUsername(user);
+            dataSource.setPassword(pass);
+            if(init >= 0){
+                dataSource.setInitialSize(init);
+            }
+
+            if(max >= 0){
+                dataSource.setMaxTotal(max);
+            }
+        }
+
+        public Connection getConnection() throws SQLException {
+            return dataSource.getConnection();
+        }
+
     }
 }
