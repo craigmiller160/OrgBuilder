8c389a3b5db653c3dd77ac726e8d6ca8d52dd340
craigmiller160 <craigmiller160@gmail.com>
2016-08-22 18:35:16 -0400

    Moved query name constants from QueryTypes to MemberJoins, and deleted QueryTypes.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/data/MemberJoins.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/MemberJoins.java
@@ -7,6 +7,9 @@ import java.util.List;
  */
 public interface MemberJoins<E,I> {
 
+    String GET_ALL_BY_MEMBER = "getAllByMember";
+    String COUNT_BY_MEMBER = "countByMember";
+
     List<E> getAllByMember(I id) throws OrgApiDataException;
 
     List<E> getAllByMember(I id, long offset, long size) throws OrgApiDataException;
