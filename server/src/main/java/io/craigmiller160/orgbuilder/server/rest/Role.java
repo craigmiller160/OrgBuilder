3b3bb205c1930618c8290da4b7ebb5e15bd08e38
craigmiller160 <craigmiller160@gmail.com>
2016-09-04 15:32:01 -0400

    Created Role enum defining security roles.

---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/rest/Role.java
@@ -0,0 +1,23 @@
+package io.craigmiller160.orgbuilder.server.rest;
+
+/**
+ * Created by craig on 9/4/16.
+ */
+public enum Role {
+
+    MASTER ("The webmaster. Has master access to the application and all organizations"),
+    ADMIN ("The administrator of an organization. Has read/write access to only the data of that organization, as well as the ability to create new user accounts for that organization."),
+    READ_WRITE ("A regular user of an organization. Has read/write access to only the data of that organization."),
+    READ_ONLY ("A regular user of an organization. Has read only access to only the data of that organization.");
+
+    private final String description;
+
+    Role(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+}
