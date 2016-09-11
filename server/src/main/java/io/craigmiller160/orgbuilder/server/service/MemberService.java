759bec24015f8f7c87286e4ca485c0f6ab99f21d
craigmiller160 <craigmiller160@gmail.com>
2016-09-11 13:57:35 -0400

    Refactored common service code out of MemberService into new ServiceCommons class.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/service/MemberService.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/service/MemberService.java
@@ -1,18 +1,15 @@
 package io.craigmiller160.orgbuilder.server.service;
 
-import io.craigmiller160.orgbuilder.server.ServerCore;
 import io.craigmiller160.orgbuilder.server.data.Dao;
 import io.craigmiller160.orgbuilder.server.data.DataConnection;
 import io.craigmiller160.orgbuilder.server.data.MemberJoins;
 import io.craigmiller160.orgbuilder.server.data.OrgApiDataException;
-import io.craigmiller160.orgbuilder.server.data.OrgDataManager;
 import io.craigmiller160.orgbuilder.server.dto.AddressDTO;
 import io.craigmiller160.orgbuilder.server.dto.DTO;
 import io.craigmiller160.orgbuilder.server.dto.EmailDTO;
 import io.craigmiller160.orgbuilder.server.dto.MemberDTO;
 import io.craigmiller160.orgbuilder.server.dto.MemberListDTO;
 import io.craigmiller160.orgbuilder.server.dto.PhoneDTO;
-import io.craigmiller160.orgbuilder.server.rest.UserOrgPrincipal;
 
 import javax.ws.rs.core.SecurityContext;
 import java.util.ArrayList;
@@ -23,18 +20,10 @@ import java.util.List;
  */
 public class MemberService {
 
-    private final String schemaName;
-    private final OrgDataManager dataManager;
-    private final SecurityContext securityContext;
+    private final ServiceCommons serviceCommons;
 
     public MemberService(SecurityContext securityContext){
-        this.dataManager = ServerCore.getOrgDataManager();
-        this.securityContext = securityContext;
-        this.schemaName = ((UserOrgPrincipal) securityContext.getUserPrincipal()).getOrg().getSchemaName();
-    }
-
-    private DataConnection newConnection() throws OrgApiDataException {
-        return dataManager.connectToSchema(schemaName);
+        this.serviceCommons = new ServiceCommons(securityContext);
     }
 
     private void insertOrUpdateJoinedEntities(MemberDTO member, MemberDTO result, DataConnection connection) throws OrgApiDataException{
@@ -74,13 +63,11 @@ public class MemberService {
     }
 
     public MemberDTO updateMember(MemberDTO member, Long id) throws OrgApiDataException, OrgApiSecurityException{
-        if(!AccessValidator.hasWriteAccess(securityContext)){
-            throw new OrgApiSecurityException("User does not have API write access. User: " + securityContext.getUserPrincipal().getName());
-        }
+        serviceCommons.hasWriteAccess();
         DataConnection connection = null;
         MemberDTO result = null;
         try{
-            connection = newConnection();
+            connection = serviceCommons.newConnection();
             Dao<MemberDTO,Long> memberDao = connection.newDao(MemberDTO.class);
 
             member.setElementId(id);
@@ -91,22 +78,20 @@ public class MemberService {
             connection.commit();
         }
         catch(OrgApiDataException ex){
-            rollback(connection, ex);
+            serviceCommons.rollback(connection, ex);
         }
         finally{
-            closeConnection(connection);
+            serviceCommons.closeConnection(connection);
         }
         return result;
     }
 
     public MemberDTO addMember(MemberDTO member) throws OrgApiDataException, OrgApiSecurityException{
-        if(!AccessValidator.hasWriteAccess(securityContext)){
-            throw new OrgApiSecurityException("User does not have API write access. User: " + securityContext.getUserPrincipal().getName());
-        }
+        serviceCommons.hasWriteAccess();
         DataConnection connection = null;
         MemberDTO result = null;
         try{
-            connection = newConnection();
+            connection = serviceCommons.newConnection();
             Dao<MemberDTO,Long> memberDao = connection.newDao(MemberDTO.class);
             result = memberDao.insert(member);
 
@@ -115,23 +100,21 @@ public class MemberService {
             connection.commit();
         }
         catch(OrgApiDataException ex){
-            rollback(connection, ex);
+            serviceCommons.rollback(connection, ex);
         }
         finally{
-            closeConnection(connection);
+            serviceCommons.closeConnection(connection);
         }
 
         return result;
     }
 
     public MemberDTO deleteMember(Long memberId) throws OrgApiDataException, OrgApiSecurityException{
-        if(!AccessValidator.hasWriteAccess(securityContext)){
-            throw new OrgApiSecurityException("User does not have API write access. User: " + securityContext.getUserPrincipal().getName());
-        }
+        serviceCommons.hasWriteAccess();
         DataConnection connection = null;
         MemberDTO result = null;
         try{
-            connection = newConnection();
+            connection = serviceCommons.newConnection();
             MemberDTO tempResult = new MemberDTO();
             deleteJoinedEntities(memberId, tempResult, connection);
 
@@ -146,22 +129,20 @@ public class MemberService {
             connection.commit();
         }
         catch(OrgApiDataException ex){
-            rollback(connection, ex);
+            serviceCommons.rollback(connection, ex);
         }
         finally{
-            closeConnection(connection);
+            serviceCommons.closeConnection(connection);
         }
         return result;
     }
 
     public MemberDTO getMember(Long memberId) throws OrgApiDataException, OrgApiSecurityException{
-        if(!AccessValidator.hasReadAccess(securityContext)){
-            throw new OrgApiSecurityException("User does not have API read access. User: " + securityContext.getUserPrincipal().getName());
-        }
+        serviceCommons.hasReadAccess();
         DataConnection connection = null;
         MemberDTO result = null;
         try{
-            connection = newConnection();
+            connection = serviceCommons.newConnection();
             Dao<MemberDTO,Long> memberDao = connection.newDao(MemberDTO.class);
             Dao<AddressDTO,Long> addressDao = connection.newDao(AddressDTO.class);
             Dao<PhoneDTO,Long> phoneDao = connection.newDao(PhoneDTO.class);
@@ -179,22 +160,20 @@ public class MemberService {
             connection.commit();
         }
         catch(OrgApiDataException ex){
-            rollback(connection, ex);
+            serviceCommons.rollback(connection, ex);
         }
         finally{
-            closeConnection(connection);
+            serviceCommons.closeConnection(connection);
         }
         return result;
     }
 
     public MemberListDTO getAllMembers(long offset, long size) throws OrgApiDataException, OrgApiSecurityException{
-        if(!AccessValidator.hasReadAccess(securityContext)){
-            throw new OrgApiSecurityException("User does not have API read access. User: " + securityContext.getUserPrincipal().getName());
-        }
+        serviceCommons.hasReadAccess();
         DataConnection connection = null;
         MemberListDTO results = null;
         try{
-            connection = newConnection();
+            connection = serviceCommons.newConnection();
             Dao<MemberDTO,Long> memberDao = connection.newDao(MemberDTO.class);
             List<MemberDTO> list = (offset >= 0 && size >= 0) ? memberDao.getAll(offset, size) : memberDao.getAll();
             if(list.size() > 0){
@@ -204,33 +183,12 @@ public class MemberService {
             connection.commit();
         }
         catch(OrgApiDataException ex){
-            rollback(connection, ex);
+            serviceCommons.rollback(connection, ex);
         }
         finally {
-            closeConnection(connection);
+            serviceCommons.closeConnection(connection);
         }
         return results;
-    }
-
-
-
-    private void rollback(DataConnection connection, OrgApiDataException ex) throws OrgApiDataException{
-        if(connection != null){
-            try{
-                connection.rollback();
-            }
-            catch(OrgApiDataException ex2){
-                ex2.addSuppressed(ex);
-                throw ex2;
-            }
-        }
-        throw ex;
-    }
-
-    private void closeConnection(DataConnection connection) throws OrgApiDataException{
-        if(connection != null){
-            connection.close();
-        }
     }
 
 }
---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/service/ServiceCommons.java
@@ -0,0 +1,61 @@
+package io.craigmiller160.orgbuilder.server.service;
+
+import io.craigmiller160.orgbuilder.server.ServerCore;
+import io.craigmiller160.orgbuilder.server.data.DataConnection;
+import io.craigmiller160.orgbuilder.server.data.OrgApiDataException;
+import io.craigmiller160.orgbuilder.server.data.OrgDataManager;
+import io.craigmiller160.orgbuilder.server.rest.UserOrgPrincipal;
+
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ * Created by craig on 9/11/16.
+ */
+public class ServiceCommons {
+
+    private final String schemaName;
+    private final OrgDataManager dataManager;
+    private final SecurityContext securityContext;
+
+    public ServiceCommons(SecurityContext securityContext){
+        this.dataManager = ServerCore.getOrgDataManager();
+        this.securityContext = securityContext;
+        this.schemaName = ((UserOrgPrincipal) securityContext.getUserPrincipal()).getOrg().getSchemaName();
+    }
+
+    public final DataConnection newConnection() throws OrgApiDataException {
+        return dataManager.connectToSchema(schemaName);
+    }
+
+    public final void rollback(DataConnection connection, OrgApiDataException ex) throws OrgApiDataException{
+        if(connection != null){
+            try{
+                connection.rollback();
+            }
+            catch(OrgApiDataException ex2){
+                ex2.addSuppressed(ex);
+                throw ex2;
+            }
+        }
+        throw ex;
+    }
+
+    public final void closeConnection(DataConnection connection) throws OrgApiDataException{
+        if(connection != null){
+            connection.close();
+        }
+    }
+
+    public void hasWriteAccess() throws OrgApiSecurityException{
+        if(!AccessValidator.hasWriteAccess(securityContext)){
+            throw new OrgApiSecurityException("User does not have API write access. User: " + securityContext.getUserPrincipal().getName());
+        }
+    }
+
+    public void hasReadAccess() throws OrgApiSecurityException{
+        if(!AccessValidator.hasReadAccess(securityContext)){
+            throw new OrgApiSecurityException("User does not have API read access. User: " + securityContext.getUserPrincipal().getName());
+        }
+    }
+
+}
