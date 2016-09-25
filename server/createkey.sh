8ee41601a0f7d7128d8a215ec8a7fe8461d02583
craigmiller160 <craigmiller160@gmail.com>
2016-09-25 16:51:19 -0400

    Modified createkey.sh and added to the .gitignore to not commit the generated keystore/key files.

--- server/createkey.sh
+++ server/createkey.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Script to generate RSA keypair in java keystore
 
-keytool -genkeypair -keyalg RSA -alias orgapikey -keysize 4096 -keystore ./orgkeystore -storetype JKS
+KEY_PATH=./src/main/resources/io/craigmiller160/orgbuilder/server
+KEYSTORE_FILE=$KEY_PATH/orgkeystore.jks
+CERT_FILE=$KEY_PATH/orgapikey.cert
+
+keytool -genkeypair -keyalg RSA -alias orgapikey -keysize 4096 -keystore $KEYSTORE_FILE -storetype JKS
+keytool -exportcert -alias orgapikey -file $CERT_FILE -keystore $KEYSTORE_FILE -rfc
