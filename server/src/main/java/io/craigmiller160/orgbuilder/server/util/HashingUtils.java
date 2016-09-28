b27611c39e038fa2209175127f33383b582bf5c4
craigmiller160 <craigmiller160@gmail.com>
2016-09-28 12:52:51 -0400

    Implemented generateSalt() method in HashingUtils and stubbed out the hashPassword() method.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/util/HashingUtils.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/util/HashingUtils.java
@@ -1,17 +1,24 @@
 package io.craigmiller160.orgbuilder.server.util;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 /**
  * Created by craigmiller on 9/28/16.
  */
 public class HashingUtils {
 
-    private static void foo(){
-        SecureRandom secureRandom = new SecureRandom();
-        //Use secureRandom to create the salt
-        //Use BCrypt to hash the password
-        //Return salt and hash for storage in the database
+    private static String generateSalt() throws NoSuchAlgorithmException{
+        SecureRandom secureRandom = SecureRandom.getInstanceStrong();
+        byte[] bytes = new byte[32];
+        secureRandom.nextBytes(bytes);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static String hashPassword(String password, String salt){
+        //TODO finish this
+        return null;
     }
 
 }
