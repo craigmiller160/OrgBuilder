acff46c5540a69194636d7517fedbe3f8f5f3154
craigmiller160 <craigmiller160@gmail.com>
2016-09-28 21:02:14 -0400

    Finished implementing RefreshTokenDTOSQLConverter.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/data/jdbc/converter/RefreshTokenDTOSQLConverter.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/data/jdbc/converter/RefreshTokenDTOSQLConverter.java
@@ -1,28 +1,68 @@
 package io.craigmiller160.orgbuilder.server.data.jdbc.converter;
 
 import io.craigmiller160.orgbuilder.server.dto.RefreshTokenDTO;
+import org.apache.commons.lang3.StringUtils;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
 
 /**
  * Created by craig on 9/27/16.
  */
 public class RefreshTokenDTOSQLConverter implements DTOSQLConverter<RefreshTokenDTO> {
 
+    private static final int UPDATE_KEY_PARAM_INDEX = 5;
+
     @Override
     public void parameterizeElement(PreparedStatement stmt, RefreshTokenDTO element) throws SQLException {
+        if(element.getElementId() > 0){
+            stmt.setLong(1, element.getElementId());
+        }
+        else{
+            stmt.setNull(1, Types.BIGINT);
+        }
 
+        if(element.getUserId() > 0){
+            stmt.setLong(2, element.getUserId());
+        }
+        else{
+            stmt.setNull(2, Types.BIGINT);
+        }
+
+        if(!StringUtils.isEmpty(element.getTokenHash())){
+            stmt.setString(3, element.getTokenHash());
+        }
+        else{
+            stmt.setNull(3, Types.VARCHAR);
+        }
+
+        if(element.getTimestamp() != null){
+            stmt.setTimestamp(4, Timestamp.valueOf(element.getTimestamp()));
+        }
+        else{
+            stmt.setNull(4, Types.TIMESTAMP);
+        }
     }
 
     @Override
     public RefreshTokenDTO parseResultSet(ResultSet resultSet) throws SQLException {
-        return null;
+        RefreshTokenDTO token = new RefreshTokenDTO();
+        token.setElementId(resultSet.getLong("token_id"));
+        token.setUserId(resultSet.getLong("user_id"));
+        token.setTokenHash(resultSet.getString("token_hash"));
+        Timestamp timestamp = resultSet.getTimestamp("expiration");
+        if(timestamp != null){
+            token.setTimestamp(timestamp.toLocalDateTime());
+        }
+
+        return token;
     }
 
     @Override
     public int getUpdateKeyParamIndex() {
-        return 0;
+        return UPDATE_KEY_PARAM_INDEX;
     }
 }
