b824d04565e64240816eade791831dc1ca7eb17d
craigmiller160 <craigmiller160@gmail.com>
2016-10-01 14:52:53 -0400

    Created JWTManager for working with the JSON Web Tokens. Created LegacyDateConverter to handle converting between the old Date and new LocalDateTime classes.

---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/util/LegacyDateConverter.java
@@ -0,0 +1,24 @@
+package io.craigmiller160.orgbuilder.server.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * A utility class to facilitate converting
+ * the new Java 8 LocalDateTime class to the
+ * legacy java.util.Date class.
+ *
+ * Created by craig on 10/1/16.
+ */
+public class LegacyDateConverter {
+
+    public static Date convertLocalDateTimeToDate(LocalDateTime localDateTime){
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDateTime convertDateToLocalDateTime(Date date){
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+}
