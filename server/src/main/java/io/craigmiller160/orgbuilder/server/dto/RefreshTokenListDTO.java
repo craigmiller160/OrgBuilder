4fb3761bf29463fcd3c6aa5b08cc943adb60ef81
craigmiller160 <craigmiller160@gmail.com>
2016-10-17 18:25:48 -0400

    Created RefreshTokenListDTO, to wrap around lists of RefreshTokenDTOs handled by Jersey.

---
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/dto/RefreshTokenListDTO.java
@@ -0,0 +1,43 @@
+package io.craigmiller160.orgbuilder.server.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by craig on 10/17/16.
+ */
+@XmlRootElement
+public class RefreshTokenListDTO {
+
+    private List<RefreshTokenDTO> tokenList;
+
+    public RefreshTokenListDTO(){}
+
+    public RefreshTokenListDTO(List<RefreshTokenDTO> tokenList){
+        this.tokenList = tokenList;
+    }
+
+    public List<RefreshTokenDTO> getTokenList() {
+        return tokenList;
+    }
+
+    public void setTokenList(List<RefreshTokenDTO> tokenList) {
+        this.tokenList = tokenList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RefreshTokenListDTO that = (RefreshTokenListDTO) o;
+
+        return tokenList != null ? tokenList.equals(that.tokenList) : that.tokenList == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return tokenList != null ? tokenList.hashCode() : 0;
+    }
+}
