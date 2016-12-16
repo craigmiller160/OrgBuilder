cc5ef7bab1a9f3f8d94868283ae9f968d23da982
craigmiller160 <craigmiller160@gmail.com>
2016-12-16 18:13:39 -0500

    Enhanced AlwaysReturnTokenResponseFilter to make it into ResponseTokenFilter, and added logic to remove the token from the response if the response is 401 or 403.

--- server/src/main/java/io/craigmiller160/orgbuilder/server/rest/ResponseTokenFilter.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/rest/ResponseTokenFilter.java
@@ -16,15 +16,22 @@ import java.io.IOException;
  * Created by craig on 12/4/16.
  */
 @Provider
-public class AlwaysReturnTokenResponseFilter implements ContainerResponseFilter{
+public class ResponseTokenFilter implements ContainerResponseFilter{
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         String requestAuthHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
         String responseAuthHeader = responseContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
-        if(!StringUtils.isEmpty(requestAuthHeader) && StringUtils.isEmpty(responseAuthHeader)){
-            responseContext.getHeaders().putSingle(HttpHeaders.AUTHORIZATION, requestAuthHeader);
+        //Remove the Authorization header/token if the status is 401 UNAUTHORIZED or 403 FORBIDDEN. Otherwise, ensure it is returned if it isn't there already
+        int status = responseContext.getStatus();
+        if(status == 401 || status == 403){
+            responseContext.getHeaders().remove(HttpHeaders.AUTHORIZATION);
+        }
+        else{
+            if(!StringUtils.isEmpty(requestAuthHeader) && StringUtils.isEmpty(responseAuthHeader)){
+                responseContext.getHeaders().putSingle(HttpHeaders.AUTHORIZATION, requestAuthHeader);
+            }
         }
     }
 }
