36324d43873a7dec16fbdcf9740e6dfe9435cc99
craigmiller160 <craigmiller160@gmail.com>
2016-12-16 18:05:19 -0500

    Added a clientOrigin property to org-props.js.

--- client/src/js/org-props.js
+++ client/src/js/org-props.js
@@ -1,4 +1,5 @@
 //TODO serverOrigin needs to be re-assigned based on dev vs prod
 var orgProps = {
-    serverOrigin: "http://localhost:8080/orgapi"
+    serverOrigin: "http://localhost:8080/orgapi",
+    clientOrigin: "http://localhost:63342/OrgBuilder/client/out/production/client"
 };
