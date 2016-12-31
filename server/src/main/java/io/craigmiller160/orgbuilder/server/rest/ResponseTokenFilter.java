b90dc81826cc5a8b8875b57b429ac9f89f0ef3d0
craigmiller160 <craigmiller160@gmail.com>
2016-12-31 08:38:26 -0500

    No longer removing the token if a 401 or 403 response

--- server/src/main/java/io/craigmiller160/orgbuilder/server/rest/ResponseTokenFilter.java
+++ server/src/main/java/io/craigmiller160/orgbuilder/server/rest/ResponseTokenFilter.java
@@ -23,15 +23,9 @@ public class ResponseTokenFilter implements ContainerResponseFilter{
         String requestAuthHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
         String responseAuthHeader = responseContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
-        //Remove the Authorization header/token if the status is 401 UNAUTHORIZED or 403 FORBIDDEN. Otherwise, ensure it is returned if it isn't there already
-        int status = responseContext.getStatus();
-        if(status == 401 || status == 403){
-            responseContext.getHeaders().remove(HttpHeaders.AUTHORIZATION);
-        }
-        else{
-            if(!StringUtils.isEmpty(requestAuthHeader) && StringUtils.isEmpty(responseAuthHeader)){
-                responseContext.getHeaders().putSingle(HttpHeaders.AUTHORIZATION, requestAuthHeader);
-            }
+        //Ensure the token is returned if it isn't there already
+        if(!StringUtils.isEmpty(requestAuthHeader) && StringUtils.isEmpty(responseAuthHeader)){
+            responseContext.getHeaders().putSingle(HttpHeaders.AUTHORIZATION, requestAuthHeader);
         }
     }
 }
