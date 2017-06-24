463cef978eb24a43b2d0a99dab7000fe5dd40fc9
craig <craigmiller160@gmail.com>
2017-06-24 15:58:47 -0400

    Stubbed out Navbar and Sidebar vue files

---
+++ client/src/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h3>This will be the navbar</h3>
+    </div>
+</template>
---
+++ client/src/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="sidebar">
+        <h3>This will be the sidebar</h3>
+    </div>
+</template>
