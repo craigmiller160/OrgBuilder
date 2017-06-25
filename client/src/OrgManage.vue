08f2ea79e2827e37c58058329701b74872e3742c
craig <craigmiller160@gmail.com>
2017-06-25 13:11:18 -0400

    Added the ability to load the list of orgs to OrgManage.vue.

--- client/src/OrgManage.vue
+++ client/src/OrgManage.vue
@@ -8,6 +8,25 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2">
                 <!-- TODO table goes here -->
+                <div class="panel panel-primary">
+                    <table class="table table-primary table-striped">
+                        <thead>
+                            <tr>
+                                <th>Org Name</th>
+                                <th>Created Date</th>
+                                <th>Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="org in orgList">
+                                <!-- TODO need orgId in the row too -->
+                                <td>{{ org.orgName }}</td>
+                                <td>{{ org.createdDate }}</td>
+                                <td>Need Stuff</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <div class="row">
@@ -25,11 +44,42 @@
         name: 'org_manage',
         data(){
             return {
-
+                orgList: []
             }
         },
         beforeMount(){
             orgbuilder.access.hasMasterAccess(this);
+        },
+        mounted(){
+            orgbuilder.api.get('orgs')
+                .done((data,status,jqXHR) => {
+                    if(jqXHR.status === 204){
+                        console.log("No orgs found on server");
+                        return;
+                    }
+
+                    this.orgList = data.orgList;
+                });
         }
     }
 </script>
+
+<style>
+
+    .table-primary > thead {
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
+        border-bottom: 1px solid #2e6da4;
+        border-right: none;
+    }
+
+    .table.table-primary > tbody > tr > td {
+        vertical-align: middle;
+    }
+
+    .table.table-primary > thead > tr > th {
+        border-bottom: none;
+    }
+
+</style>
