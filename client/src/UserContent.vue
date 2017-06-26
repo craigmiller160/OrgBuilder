65b1c053f826c8c1da185b31c14d02864118e5ca
craig <craigmiller160@gmail.com>
2017-06-26 18:15:58 -0400

    Built the core of UserContent.vue.

---
+++ client/src/UserContent.vue
@@ -0,0 +1,170 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h2>{{ title }}</h2>
+            </div>
+        </div>
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-primary form-group">
+                        <div class="panel-heading clearfix">
+                            <h4 class="pull-left">User Info</h4>
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="content-name">Email:</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p v-show="!edit">{{ user.userEmail }}</p>
+                                    <input v-show="edit" name="userEmail" class="form-control" type="email" required v-model="user.userEmail" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="content-name">First Name:</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p v-show="!edit">{{ user.firstName }}</p>
+                                    <input v-show="edit" name="firstName" class="form-control" type="text" v-model="user.firstName" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="content-name">Last Name:</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p v-show="!edit">{{ user.lastName }}</p>
+                                    <input v-show="edit" name="lastName" class="form-control" type="text" v-model="user.lastName" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-primary form-group">
+                        <div class="panel-heading clearfix">
+                            <h4 class="pull-left">Password</h4>
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="content-name">Password:</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p v-show="!edit">************</p>
+                                    <input v-show="edit" name="password" class="form-control" type="password" required v-model="user.password" /> <!-- TODO make sure this doesn't risk compromising the password somehow... -->
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p class="content-name">Repeat Password:</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input v-show="edit" name="repeatPassword" class="form-control" type="password" required v-model="user.repeatPassword" /> <!-- TODO make sure this doesn't cause problems -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-primary form-group">
+                        <div class="panel-heading clearfix">
+                            <h4 class="pull-left">Access Roles</h4>
+                        </div>
+                        <div class="panel-body">
+                            <!-- TODO checkboxes go here -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-primary form-group">
+                        <div class="panel-heading clearfix">
+                            <h4 class="pull-left">Org</h4>
+                        </div>
+                        <div class="panel-body">
+                            <!-- TODO this is where the org selector goes -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                    <a class="btn btn-primary" type="button" title="Cancel changes">Cancel</a>
+                    <button v-show="edit" class="btn btn-success" type="submit" title="Save changes">Save</button>
+                    <a v-show="canEdit" class="btn btn-danger pull-right" type="button" title="Delete User">Delete</a>
+                    <a v-show="canEdit && !edit" class="btn btn-info pull-right" type="button" title="Edit User">Edit</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import { orgbuilder } from './js/orgbuilder.js';
+
+    export default {
+        name: 'user_content',
+        data(){
+            return {
+                title: 'New User',
+                user: {
+                    userEmail: '',
+                    password: '',
+                    roles: [],
+                    orgId: 0,
+                    orgName: null,
+                    firstName: '',
+                    lastName: '',
+                    userId: 0
+                },
+                edit: false
+            }
+        },
+        computed: {
+            canEdit(){
+                return this.$route.query.userId !== undefined && (orgbuilder.jwt.hasRole(orgbuilder.jwt.roles.master) || orgbuilder.jwt.hasRole(orgbuilder.jwt.roles.admin)) ||
+                    (this.$route.query.userId == orgbuilder.jwt.getTokenPayload().uid); //TODO clean this up
+            }
+        },
+        beforeMount(){
+            orgbuilder.access.start(this)
+                .hasAnyRole(orgbuilder.jwt.roles.master, orgbuilder.jwt.roles.admin)
+                .next()
+                .isUser(this.$route.query.userId)
+                .validate();
+
+        },
+        mounted(){
+            if(this.$route.query.userId !== undefined){
+                var app = this;
+                orgbuilder.api.get('users/' + this.$route.query.userId)
+                    .done((user, status, jqXHR) => {
+                        if(jqXHR.status === 204){
+                            console.log('User not found on server');
+                            app.$emit('showAlert', {
+                                show: true,
+                                msg: 'User not found on server',
+                                clazz: 'alert-danger'
+                            });
+                        }
+
+                        app.user = user;
+                        this.title = user.userEmail;
+                    })
+                    .fail((jqXHR) => console.log('FAILED TO RETRIEVE USER DETAILS: ' + jqXHR.status));
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
