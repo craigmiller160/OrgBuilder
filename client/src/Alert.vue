a3d8c3831bb37dcec9b6c46d74130ef469f3c50e
craig <craigmiller160@gmail.com>
2017-07-09 15:33:38 -0400

    Fixed the close link in Alert.vue so that it has a dynamic href based on the current routed URI.

--- client/src/Alert.vue
+++ client/src/Alert.vue
@@ -3,7 +3,7 @@
         <div v-show="alert.show" class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2">
                 <div id="actionAlert" class="alert fade">
-                    <a href="#" class="close" data-hide="alert" aria-label="close">&times;</a>
+                    <a :href="closeLink" class="close" data-hide="alert" aria-label="close">&times;</a>
                     <p>{{ alert.msg }}</p>
                 </div>
             </div>
@@ -20,6 +20,11 @@
         data() {
             return {
 
+            }
+        },
+        computed: {
+            closeLink(){
+                return '/#' + this.$route.fullPath;
             }
         },
         watch: {
